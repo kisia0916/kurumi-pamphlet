217e904a9d42c0c3750d68cc15fab5482828d0b7
kisia0916 <fumiharuabe@gmail.com>
2025-09-18 00:02:55 +0900

    change status cache time

--- app/api/_utils/cacheHeaders.ts
+++ app/api/_utils/cacheHeaders.ts
@@ -2,5 +2,5 @@ export const cacheSuccessHeaders = {
   "Cache-Control": "public, max-age=3600, stale-while-revalidate=59",
 };
 export const cacheSuccessHeadersMini = {
-  "Cache-Control": "public, max-age=7, stale-while-revalidate=59",
+  "Cache-Control": "public, max-age=300, stale-while-revalidate=59",
 };
